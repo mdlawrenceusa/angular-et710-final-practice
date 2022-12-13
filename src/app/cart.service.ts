25183f564d37c8fdbc4135d166d0b99c5eb3d71e
mdlawrenceusa <mdlawrence@me.com>
2022-12-13 11:18:50 -0500

    1 file modified

--- src/app/cart.service.ts
+++ src/app/cart.service.ts
@@ -24,6 +24,9 @@ export class CartService {
     return this.items;
   }
 
+  getShippingPrices() {
+    return this.http.get<{type: string, price: number}[]>('/assets/shipping.json');
+  }
 
   constructor(
 
